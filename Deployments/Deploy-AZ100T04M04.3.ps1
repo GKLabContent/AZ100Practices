4a8f3e1f7ae4bb4f408a6c93748ab26581b56579
Tracy Wallace <tswwci@hotmail.com>
2018-12-07 19:05:10 +0000

    Added deployment for T04.3

---
+++ Deployments/Deploy-AZ100T04M04.3.ps1
@@ -0,0 +1,11 @@
+﻿$uri = "https://raw.githubusercontent.com/GKLabContent/AZ100Practices/master/Deployments/AZ100T04.3.json"
+$rg = "AZ100T04M03"
+$location = "EastUS"
+
+$rgObject = Get-AzureRmResourceGroup -Name $rg -ErrorAction SilentlyContinue
+
+if ($rgObject -eq $null) {
+    New-AzureRmResourceGroup -Name $rg -Location $location
+}
+
+New-AzureRmResourceGroupDeployment -Name "Setup" -ResourceGroupName $rg -TemplateUri $uri -Mode Incremental -Force -Verbose
